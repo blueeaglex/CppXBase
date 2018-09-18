768818270bc1e5980c6f3ee2ee89b98c577774d3
Blue Eagle <1041151713@qq.com>
2018-09-18 16:21:33 +0800

    Project Start
    Lanch the Cpp Cross-platform Base project

---
+++ Base/console.h
@@ -0,0 +1,29 @@
+﻿#include "os.h"
+
+namespace console
+{
+	
+inline void checkOutputDir(String &directory, bool no_abort = false)
+{
+	if (os::filesystem::exists(directory))
+	{
+		if (no_abort)
+		{
+			std::cout << "已覆盖文件夹 <" << directory << ">" << std::endl;
+		}
+		else
+		{
+			std::cout << "文件夹 <" << directory << "> 已经存在,继续可能覆盖原数据,是否继续？[y/n]" << std::endl;
+			String user_confirm;
+		reconfirm:
+			std::cin >> user_confirm;
+			if ("n" == user_confirm)
+				exit(0);
+			else if ("y" != user_confirm)
+				goto reconfirm;
+		}
+	}
+	else os::filesystem::create_directories(directory);
+}
+
+}
