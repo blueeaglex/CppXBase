d22b8b6f44c1c6f36ca8d7b2cf32c7d60537d7b0
blueeaglex <1041151713@qq.com>
2018-09-20 19:45:10 +0800

    Fix boost link errors

--- include/Base/os.h
+++ include/Base/os.h
@@ -1,19 +1,22 @@
-#ifndef __OS_H__
-#define __OS_H__
-#include "boost/filesystem.hpp"
+#ifndef __CPP_BASE_OS_H__
+#define __CPP_BASE_OS_H__
+
+#include <boost/filesystem.hpp>
 #include "boost/chrono.hpp"
 #include "boost/interprocess/file_mapping.hpp"
 #include "boost/interprocess/mapped_region.hpp"
+#include "boost/process.hpp"
 #include <string>
 #define _CRT_SECURE_NO_WARNINGS
 namespace os
 {
 	namespace filesystem = boost::filesystem;
-	namespace chrono = boost::chrono;
-	namespace interprocess = boost::interprocess;
+    namespace chrono = boost::chrono;
+    namespace interprocess = boost::interprocess;
+    namespace process = boost::process;
 	static const size_t MaxPath = 260u;
 }
 
 typedef std::string String;
 
-#endif //__OS_H__
+#endif //__CPP_BASE_OS_H__
