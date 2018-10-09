ee1fcb22ccaed446a16d041f54b1dc07171e1ce0
blueeaglex <1041151713@qq.com>
2018-10-09 10:47:49 +0800

    Process operations support

--- include/Base/log.h
+++ include/Base/log.h
@@ -1,12 +1,21 @@
 #ifndef __LOG_H__
 #define __LOG_H__
 
-#include "boost/log/trivial.hpp"
 
-#define DEBUG_LOG BOOST_LOG_TRIVIAL(debug) << __FUNCTION__ << ":" << __LINE__
-#define INFO_LOG  BOOST_LOG_TRIVIAL(info) << __FUNCTION__ << ":" << __LINE__
-#define WARNING_LOG BOOST_LOG_TRIVIAL(warning) << __FUNCTION__ << ":" << __LINE__
-#define ERROR_LOG BOOST_LOG_TRIVIAL(error) << __FUNCTION__ << ":" << __LINE__
-#define FATAL_LOG BOOST_LOG_TRIVIAL(fatal) << __FUNCTION__ << ":" << __LINE__
+#ifdef OS_WINDOWS
+#  include "boost/log/trivial.hpp"
+#  define DEBUG_LOG BOOST_LOG_TRIVIAL(debug) << __FUNCTION__ << ":" << __LINE__
+#  define INFO_LOG  BOOST_LOG_TRIVIAL(info) << __FUNCTION__ << ":" << __LINE__
+#  define WARNING_LOG BOOST_LOG_TRIVIAL(warning) << __FUNCTION__ << ":" << __LINE__
+#  define ERROR_LOG BOOST_LOG_TRIVIAL(error) << __FUNCTION__ << ":" << __LINE__
+#  define FATAL_LOG BOOST_LOG_TRIVIAL(fatal) << __FUNCTION__ << ":" << __LINE__
+#else
+#  include <iostream>
+#  define DEBUG_LOG   std::cout << "[DEBUG]" << __FUNCTION__ << ":" << __LINE__ << " "
+#  define INFO_LOG    std::cout << "[INFO]" << __FUNCTION__ << ":" << __LINE__ << " "
+#  define WARNING_LOG std::cout << "[WARNING]" << __FUNCTION__ << ":" << __LINE__ << " "
+#  define ERROR_LOG   std::cout << "[ERROR]" << __FUNCTION__ << ":" << __LINE__ << " "
+#  define FATAL_LOG   std::cout << "[FATAL]" << __FUNCTION__ << ":" << __LINE__ << " "
+#endif
 
 #endif
